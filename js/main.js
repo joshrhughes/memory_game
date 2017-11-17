bf130fe2ac6ea16c0dcde1a8d577479aa922596f
joshrhughes <josh@mtnpine.us>
2017-11-17 10:05:28 -0700

    added game logic.  Think there is still an issue with checkForMatch

--- js/main.js
+++ js/main.js
@@ -1,21 +1,38 @@
-console.log("Up and running!");
+//console.log("Up and running!");
 
 
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
 
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0]);
-
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) 
-	{ 
-		if(cardsInPlay[0] === cardsInPlay[1])
-			{alert("You found a match!")}
-		else
-			{alert("Sorry, try again.")}		
+var checkForMatch = function()
+	{
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+		} else {
+		console.log("Sorry, try again.");
+		}
 	}
+
+var flipCard = function(cardId)
+	{
+		checkForMatch()
+		console.log("user flipped " + cards[cardId])
+		cardsInPlay.push(cards[cardId])
+
+		if (cardsInPlay.length === 2) 
+			{ 
+				checkForMatch()	
+			}
+
+	}
+	flipCard(0)
+	flipCard(2)
+
+
+/*var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo); */
